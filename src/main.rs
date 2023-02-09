42dec19c82e7ddd764933442b0445b18a444de56
prajnastra <coder.abhijit90@gmail.com>
2023-02-09 18:24:08 +0530

    add: cli arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+
+    #[arg(short, long, default_value_t = 1.0)]
+    volume: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("path {}!", args.path);
+    println!("volume {}!", args.volume);
 }
