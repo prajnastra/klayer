82a69b275786067335056a1414862a286bc838e6
prajnastra <coder.abhijit90@gmail.com>
2023-02-09 19:05:18 +0530

    add: volume control

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
-use rodio::{source::Source, Decoder, OutputStream};
+use rodio::{Decoder, OutputStream, Sink};
 use std::fs::File;
 use std::io::BufReader;
-use std::thread::sleep;
-use std::time::Duration;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -11,7 +9,7 @@ struct Args {
     #[arg(short, long)]
     path: String,
 
-    #[arg(short, long, default_value_t = 1.0)]
+    #[arg(short, long, default_value_t = 0.6)]
     volume: f32,
 }
 
@@ -19,10 +17,13 @@ fn main() {
     let args = Args::parse();
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    // add a dummu source
     let file = BufReader::new(File::open(args.path).unwrap());
     let source = Decoder::new(file).unwrap();
 
-    stream_handle.play_raw(source.convert_samples()).unwrap();
-
-    sleep(Duration::from_secs(2));
+    sink.append(source);
+    sink.set_volume(args.volume);
+    sink.sleep_until_end();
 }
